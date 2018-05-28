b8b8234f57f323dcd7f9f0872305e88348bee4cd
Alan Russell <ajrussellaudio@gmail.com>
2018-05-28 11:15:57 +0100

    add class Alistair

---
+++ src/main/java/Alistair.java
@@ -0,0 +1,16 @@
+public class Alistair {
+    private final String name = "Big Daddy Kane";
+    private int age;
+
+    public Alistair(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/test/java/AlistairTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class AlistairTest {
+    private Alistair alistair;
+
+    @Before
+    public void setUp() throws Exception {
+        alistair = new Alistair(26);
+    }
+
+    @Test
+    public void isValidObject() {
+        assertNotNull(alistair);
+    }
+
+    @Test
+    public void hasAge() {
+        assertEquals(26, alistair.getAge());
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Big Daddy Kane", alistair.getName());
+    }
+}
